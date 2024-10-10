f77e66ee09d0fb33f6b43823ea47d417f1a9a65a
daniel <daniel@dragon.home1.net>
2024-10-10 10:04:30 -0400

    services

--- pibuild.sh
+++ pibuild.sh
@@ -7,77 +7,82 @@ function pause()
 
 function basepackages()
 {
-apt -y install git
-apt -y install gcc
-apt -y install g++
-apt -y install make
-apt -y install cmake
-apt -y install libasound2-dev
-apt -y install libudev-dev
-apt -y install libavahi-client-dev
-apt -y install alsa-utils
-apt -y install acl
-apt -u install zlib1g-dev
+  apt -y install git
+  apt -y install gcc
+  apt -y install g++
+  apt -y install make
+  apt -y install cmake
+  apt -y install libasound2-dev
+  apt -y install libudev-dev
+  apt -y install libavahi-client-dev
+  apt -y install alsa-utils
+  apt -y install acl
+  apt -u install zlib1g-dev
 
-pause 
+  pause 
 }
 
 function installdw()
 {
-cd ~
-git clone https://www.github.com/wb2osz/direwolf
-cd direwolf
-git checkout dev
-mkdir build && cd build
-cmake ..
-make -j4
-sudo make install
-make install-conf
-cd ~
-pause
+  cd ~
+  git clone https://www.github.com/wb2osz/direwolf
+  cd direwolf
+  git checkout dev
+  mkdir build && cd build
+  cmake ..
+  make -j4
+  sudo make install
+  make install-conf
+  cd ~
+  pause
 }
 
 function configaccess()
 {
-read -p ' Enter Username: ' username
+  read -p ' Enter Username: ' username
 
-groupadd packetradio
-usermod -a -G dialout $username
-usermod -aG pulse-access $username
-usermod -aG audio $username
-usermod -aG plugdev $username
-usermod -aG packetradio $username
-useradd prad
-usermod -aG dialout prad
-usermod -aG pulse-access prad
-usermod -aG audio prad
-usermod -aG plugdev prad
-usermod -aG packetradio prad
+  groupadd packetradio
+  usermod -a -G dialout $username
+  usermod -aG pulse-access $username
+  usermod -aG audio $username
+  usermod -aG plugdev $username
+  usermod -aG packetradio $username
+  useradd prad
+  usermod -aG dialout prad
+  usermod -aG pulse-access prad
+  usermod -aG audio prad
+  usermod -aG plugdev prad
+  usermod -aG packetradio prad
 
-pause
+  pause
 }
 
 function configbbs()
 {
-mkdir /opt/direwolf
-mkdir /opt/linbpq
-mkdir /opt/hamtrek
-setfacl -d -m g:packetradio:rwx /opt/direwolf
-setfacl -d -m g:packetradio:rwx /opt/linbpq
-setfacl -d -m g:packetradio:rwx /opt/hamtrek
+  mkdir /opt/direwolf
+  setfacl -d -m g:packetradio:rwx /opt/direwolf
+  setfacl -m g:packetradio:rwx /opt/direwolf
+  mkdir /opt/direwolf/log
+  mkdir /opt/linbpq
+  setfacl -d -m g:packetradio:rwx /opt/linbpq
+  setfacl -m g:packetradio:rwx /opt/linbpq
+  mkdir /opt/linbpq/log
+  mkdir /opt/hamtrek
+  setfacl -d -m g:packetradio:rwx /opt/hamtrek
+  setfacl -m g:packetradio:rwx /opt/hamtrek
 
-cd ~
-git clone https://github.com/g8bpq/linbpq
-git clone https://github.com/tekkon7tech/hamtrek
-apt -y install libconfig-dev
-apt -y install libpcap-dev
-apt -y install libminiupnpc-dev
-cd ~/linbpq
-make
-cp ~/linbpq/linbpq /opt/linbpq
-cd ~/hamtrek/src
-make
-cp ~/hamtrek/src/hamtrek /opt/hamtrek
+  cd ~
+  git clone https://github.com/g8bpq/linbpq
+  git clone https://github.com/tekkon7tech/hamtrek
+  apt -y install libconfig-dev
+  apt -y install libpcap-dev
+  apt -y install libminiupnpc-dev
+  cd ~/linbpq
+  make
+  cp ~/linbpq/linbpq /opt/linbpq
+  cd ~/hamtrek/src
+  make
+  cp ~/hamtrek/src/hamtrek /opt/hamtrek
 }
 
 function menu()
